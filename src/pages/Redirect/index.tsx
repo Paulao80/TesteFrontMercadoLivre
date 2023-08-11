72036d6551bcd048a73f2ba807397e8746d6f8ed
Paulo Vinicius <pauloviniciusjipa@gmail.com>
2023-08-11 16:27:54 -0400

    Info: Adicionado a requisição a api do ml

--- src/pages/Redirect/index.tsx
+++ src/pages/Redirect/index.tsx
@@ -1,14 +1,36 @@
 import useQuery from "../../hooks/useQuery";
+import { apiML } from "../../apis/apiML";
 
 const Redirect = () => {
   const query = useQuery();
+  const clientId = import.meta.env.VITE_CLIENT_ID;
+  const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 
-  const code = query.get("code");
-  console.log("🚀 ~ file: index.tsx:7 ~ Redirect ~ code:", code);
-  const state = query.get("state");
-  console.log("🚀 ~ file: index.tsx:9 ~ Redirect ~ state:", state);
+  const onClick = async () => {
+    const code = query.get("code");
 
-  return <div>Hello world!</div>;
+    if (!code) return;
+    if (!clientId) return;
+    if (!clientSecret) return;
+
+    const params = new URLSearchParams();
+
+    params.append("grant_type", "authorization_code");
+    params.append("client_id", clientId);
+    params.append("client_secret", clientSecret);
+    params.append("code", code);
+    params.append("redirect_uri", location.href);
+
+    const resp = await apiML.post("/oauth/token", params);
+    console.log("🚀 ~ file: index.tsx:25 ~ onClick ~ resp:", resp);
+  };
+
+  return (
+    <>
+      <div>Redirect</div>
+      <button onClick={onClick}>Obtem token</button>
+    </>
+  );
 };
 
 export default Redirect;
