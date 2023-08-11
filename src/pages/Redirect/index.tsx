b5a3a43ff3f632b8b11e29c9987ae04171ea921c
Paulo Vinicius <pauloviniciusjipa@gmail.com>
2023-08-11 14:17:30 -0400

    Info: Alterado a page redirect

--- src/pages/Redirect/index.tsx
+++ src/pages/Redirect/index.tsx
@@ -1,9 +1,10 @@
 import { useParams } from "react-router-dom";
+
 const Redirect = () => {
   const params = useParams();
   console.log("🚀 ~ file: index.tsx:4 ~ Redirect ~ params:", params);
 
-  return <></>;
+  return <div>Hello world!</div>;
 };
 
 export default Redirect;
